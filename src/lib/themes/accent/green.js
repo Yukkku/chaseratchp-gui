179358e492ebd5a5f2cd8a33a882de70b0d7a687
Yukkku <yukku.scratcher@gmail.com>
2025-09-03 14:46:29 +0900

    差別化のため色を緑色に変更

---
+++ src/lib/themes/accent/green.js
@@ -0,0 +1,13 @@
+const guiColors = {
+    'looks-secondary': 'hsla(95, 100%, 35%, 1)',
+    'looks-transparent': 'hsla(95, 100%, 35%, 0.35)',
+    'looks-light-transparent': 'hsla(95, 100%, 35%, 0.15)',
+    'looks-secondary-dark': 'hsla(95, 60%, 30%, 1)'
+};
+
+const blockColors = {};
+
+export {
+    guiColors,
+    blockColors
+};
